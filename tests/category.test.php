04dea4e4c9126a3695aea641cc496a58ff8618e9
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-01-19 21:33:53 +0100

    created test for update category method

--- tests/category.test.php
+++ tests/category.test.php
@@ -2,9 +2,16 @@
 
 require_once __DIR__.'/../classes/Category.php';
 
-$category = new Category(null, 'test');
+// $category = new Category(null, 'test');
 
-if($category->create()){
+// if($category->create()){
+//     echo 'success';
+// }else{
+//     echo 'failed';
+// }
+
+$category = new Category(1, 'test1');
+if($category->update()){
     echo 'success';
 }else{
     echo 'failed';
