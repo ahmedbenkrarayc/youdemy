cc3626ebcf94d8a86ca387e28b3e54da2539782e
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-01-19 20:22:33 +0100

    created file to get current user

--- views/auth/user.php
+++ views/auth/user.php
@@ -2,7 +2,7 @@
 require_once __DIR__.'/../../classes/Etudiant.php';
 if(isset($_SESSION['user_id'])){
     $user = new Etudiant($_SESSION['user_id'], null, null, null, null);
-    $authUser = $user->getUser();
+    $authUser = $user->currentUser();
 }else{
     $authUser = null;
 }
