554dfd9bc2b43b2a3df1ff2ed67f9af611e9853b
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-01-19 14:52:17 +0100

    overriding register method in enseignant class

--- classes/Enseignant.php
+++ classes/Enseignant.php
@@ -1,13 +1,13 @@
 <?php
-
 require_once __DIR__.'/Etudiant.php';
 
 class Enseignant extends Etudiant{
     private $status;
 
     public function __construct($id, $fname, $lname, $email, $password, $status = null, $suspended = null, $createdAt = null, $updatedAt = null){
-        parent::__construct($id, $fname, $lname, $email, $password, 'enseignant', $suspended, $createdAt, $updatedAt);
         try{
+            parent::__construct($id, $fname, $lname, $email, $password, $suspended = null, $createdAt = null, $updatedAt = null);
+            $this->setRole('enseignant');
             $this->setStatus($status);
         }catch(InputException $e){
             array_push($this->errors, $e->getMessage());
@@ -33,7 +33,74 @@ class Enseignant extends Etudiant{
     }
 
     public function register(){
+        try{
+            $nullvalue = false;
+            if($this->getFname() == null){
+                $this->errors[] = 'First name is required !';
+                $nullvalue = true;
+            }
 
+            if($this->getLname() == null){
+                $this->errors[] = 'Last name is required !';
+                $nullvalue = true;
+            }
+
+            if($this->getEmail() == null){
+                $this->errors[] = 'Email is required !';
+                $nullvalue = true;
+            }
+            
+            if($this->getPassword() == null){
+                $this->errors[] = 'Password is required !';
+                $nullvalue = true;
+            }
+
+            if($this->getRole() == null){
+                $this->errors[] = 'Role is required !';
+                $nullvalue = true;
+            }
+
+            if($nullvalue)
+                return false;
+
+            $connection = Database::getInstance()->getConnection();
+            $emailCheckQuery = 'SELECT COUNT(*) FROM user WHERE email = :email';
+            $emailCheckStmt = $connection->prepare($emailCheckQuery);
+            $emailCheckStmt->bindValue(':email', $this->getEmail(), PDO::PARAM_STR);
+            $emailCheckStmt->execute();
+            if ($emailCheckStmt->fetchColumn() > 0) {
+                $this->errors[] = 'Email already exists!';
+                return false;
+            }
+
+            $hashedPassword = password_hash($this->password, PASSWORD_DEFAULT);
+            
+            $query = 'INSERT INTO user(fname, lname, email, password, role) VALUES(:fname, :lname, :email, :password, :role)';
+            $stmt = $connection->prepare($query);
+            $stmt->bindValue(':fname', htmlspecialchars($this->fname), PDO::PARAM_STR);
+            $stmt->bindValue(':lname', htmlspecialchars($this->lname), PDO::PARAM_STR);
+            $stmt->bindValue(':email', htmlspecialchars($this->email), PDO::PARAM_STR);
+            $stmt->bindValue(':password', $hashedPassword, PDO::PARAM_STR);
+            $stmt->bindValue(':role', htmlspecialchars($this->role), PDO::PARAM_STR);
+            if($stmt->execute()){
+                $id = $connection->lastInsertId();
+                $query = 'INSERT INTO enseignant(id) VALUES(:id)';
+                unset($stmt);
+                $stmt = $connection->prepare($query);
+                $stmt->bindValue(':id', $id, PDO::PARAM_STR);
+                if($stmt->execute()){
+                    return true;
+                }
+
+            }
+
+            $this->errors[] = 'Something went wrong !';
+            return false;
+        }catch(PDOException $e){
+            Logger::error_log($e->getMessage());
+            $this->errors[] = 'Something went wrong !';
+            return false;
+        }
     }
 
     public function suspendAccount(){
