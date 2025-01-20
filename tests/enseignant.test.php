386300a490b1c5eddceec0281e56f036fb216b71
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-01-20 12:15:48 +0100

    created test for getAll method of enseignant

--- tests/enseignant.test.php
+++ tests/enseignant.test.php
@@ -4,5 +4,7 @@ require_once __DIR__.'/../classes/Enseignant.php';
 
 $user = new Enseignant(null, 'ahmed', 'benkrara', 'ahmed.benkrara12@mail.com', 'ahmed123');
 
-echo $user->register();
-print_r($user->getErrors());
+// echo $user->register();
+// print_r($user->getErrors());
+
+print_r($user->getAll());
